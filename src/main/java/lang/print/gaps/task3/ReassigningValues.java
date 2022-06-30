09df2c89256564de0ded9c766dfb1445504aba06
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-06-30 15:50:02 +0400

    completed tasks

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
+        int first = 1, second = 10, third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
+        int linkToFirst = first, linkToSecond = second, linkToThird = third;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println(first + "\n" + second + "\n" + third);
+        System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
     }
 }
